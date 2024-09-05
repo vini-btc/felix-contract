f3cb07e32fc405f2148e72dcc208e32b37077dbc
Vini Andrade <me@vicnicius.com>
2024-09-05 17:34:41 +0200

    Remove unused import
    Signed-off-by: Vini Andrade <me@vicnicius.com>

--- tests/claim-funds.test.ts
+++ tests/claim-funds.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, it } from "vitest";
-import { cvToValue, principalCV, trueCV, uintCV } from "@stacks/transactions";
+import { principalCV, trueCV, uintCV } from "@stacks/transactions";
 
 const accounts = simnet.getAccounts();
 
