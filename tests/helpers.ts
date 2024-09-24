76ac162c258fb5e44a513d37a06277e2e910916b
Vini Andrade <me@vicnicius.com>
2024-09-24 06:23:24 +0200

    Remove unused test helpers
    Signed-off-by: Vini Andrade <me@vicnicius.com>

--- tests/helpers.ts
+++
@@ -1,8 +0,0 @@
-export const range = (start: number, end: number) =>
-  Array.from({ length: end - start }, (_, k) => k + start);
-
-export function getContractAddress(): string {
-  return `${simnet.getAccounts().get("deployer")!}.felix-ticket`;
-}
-
-export const felixPrincipalAddress = "STNHKEPYEPJ8ET55ZZ0M5A34J0R3N5FM2CMMMAZ6";
